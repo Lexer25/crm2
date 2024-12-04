6ca7a8d49d849dd87a34e4fc657358e107961e72
Lexer25 <b71@mail.ru>
2024-12-04 10:39:17 +0300

    версия 3.6.17

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -140,7 +140,7 @@ Kohana::modules(array(
 	'cards' => MODPATH . 'cards', // подключение модуля cards
 	'contacts' => MODPATH . 'contacts', // подключение модуля contacts
 	
-	//'passoffice' => MODPATH . 'passoffice', // подключение модуля Passoffice
+	'passoffice' => MODPATH . 'passoffice', // подключение модуля Passoffice
 	'kohana-acl' => MODPATH . 'kohana-acl', // подключение модуля Passoffice
 	'menu' => MODPATH.'menu',
 	'mreports' => MODPATH.'report',
