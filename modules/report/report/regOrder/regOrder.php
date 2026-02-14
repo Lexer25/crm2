e3607f8fb2069421bde3c8442919e342e411f152
Андрей <andrey.buharov@gmail.com>
2026-02-15 00:33:39 +0300

    Отчет по бюро пропусков стал работать быстро.

--- modules/report/report/regOrder/regOrder.php
+++ modules/report/report/regOrder/regOrder.php
@@ -84,35 +84,30 @@ class Model_Report_regOrder extends Model_Report_Base
 				$timeStart=time(true);
 				
 								
-				$sql='select p.surname||\' \'|| p.name||\' \'|| p.patronymic as fio, count(*) from guestorder go
-						join people p on p.id_pep=go.id_pep
-						where go.timeorder >\''.Arr::get($post, 'reportdatestart').'\'
-						and go.timeorder<\''.Arr::get($post, 'reportdateend').'\'
-						group by 1';
-						
-			
-			
-			$query = DB::query(Database::SELECT,
-				'SELECT 
-						p.surname || \' \' || p.name || \' \' || p.patronymic as fio, 
-						 count(distinct go.id_guestorder) as ordered_count,  -- уникальные заказы
-						count(distinct e.id_card) as issued_count  -- количество выданных пропусков
-					FROM guestorder go
-					JOIN people p ON p.id_pep = go.id_pep
-					LEFT JOIN events e ON go.id_guest = e.ess1 
-						AND e.datetime BETWEEN :date_start AND :date_end
-						AND e.id_eventtype IN (17)
-					WHERE go.timeorder BETWEEN :date_start AND :date_end
-					GROUP BY 1
-					ORDER BY 1')
+									
+				$query = DB::query(Database::SELECT,
+				'SELECT p.id_pep,             p.surname || \' \' || p.name || \' \' || p.patronymic as fio,
+                         count(distinct go.id_guestorder) as ordered_count  ,
+                         count(distinct e.id_card) as ordered_count
+                    FROM guestorder go
+                    JOIN people p ON p.id_pep = go.id_pep
+                    JOIN events e ON go.id_guest = e.ess1
+                        AND e.datetime BETWEEN :date_start AND :date_end
+                        AND e.id_eventtype IN (17)
+                    WHERE go.timeorder BETWEEN :date_start AND :date_end
+                    and go.timeorder BETWEEN :date_start AND :date_end
+                    GROUP BY 1, 2
+                    ORDER BY 2')
 				->param(':date_start', Arr::get($post, 'reportdatestart'))
-					->param(':date_end', Arr::get($post, 'reportdateend'))
-					->param(':pep_id', 33719);
+				->param(':date_end', Arr::get($post, 'reportdateend'))
+				//	;
+					
+					
 				
 				
-				echo Debug::vars('113', $query->compile(Database::instance('fb')));exit;
-				//->execute(Database::instance('fb'));
-				//->as_array();
+				//echo Debug::vars('113', $query->compile(Database::instance('fb')));exit;
+				->execute(Database::instance('fb'))
+				->as_array();
 				
 				foreach ($query as $key=>$value)
 				{
